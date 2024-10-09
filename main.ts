0fc0f001992b377b18bd0f915c511976015ab6d5
Eka Wahyu <94496984+kwhyu@users.noreply.github.com>
2024-10-09 20:26:10 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,17 @@
-// main.ts
+// deploy.ts
 import { serve } from "https://deno.land/std@0.152.0/http/server.ts";
 
-const server = serve({ port: 8000 });
-console.log("Server berjalan di http://localhost:8000");
+const port = parseInt(Deno.env.get("PORT") || "8000", 10);
+const server = serve({ port });
+console.log(`Server running on http://localhost:${port}`);
 
 for await (const req of server) {
   const url = new URL(req.url);
 
-  // Mengatur rute
   if (url.pathname === "/") {
-    // Mengirimkan isi file HTML
     const html = await Deno.readTextFile("index.html");
     req.respond({ body: html, headers: new Headers({ "content-type": "text/html; charset=utf-8" }) });
   } else {
-    // Menangani rute yang tidak dikenal
     req.respond({ status: 404, body: "404 Not Found" });
   }
 }
